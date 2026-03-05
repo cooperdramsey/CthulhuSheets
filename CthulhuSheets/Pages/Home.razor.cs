a2bd8a8b8f5aacd5abe668c3f29618f572e31453
cooperdramsey <cooperdramsey@gmail.com>
2026-03-05 10:08:16 -0500

    Add default investigator

--- CthulhuSheets/Pages/Home.razor.cs
+++ CthulhuSheets/Pages/Home.razor.cs
@@ -3,10 +3,25 @@ namespace CthulhuSheets.Pages;
 public partial class Home : IDisposable
 {
     [Inject] private InvestigatorService InvestigatorService { get; set; } = default!;
+    [Inject] private HttpClient Http { get; set; } = default!;
+
+    private static readonly JsonSerializerOptions JsonOptions = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
 
     protected override void OnInitialized()
     {
         InvestigatorService.OnChanged += StateHasChanged;
+    }
+
+    private async Task LoadSampleAsync()
+    {
+        var json = await Http.GetStringAsync("samples/Dr.-Eleanor-Whitmore.json");
+        var investigator = JsonSerializer.Deserialize<Investigator>(json, JsonOptions);
+        if (investigator is not null)
+            await InvestigatorService.LoadAsync(investigator);
     }
 
     public void Dispose() => InvestigatorService.OnChanged -= StateHasChanged;
